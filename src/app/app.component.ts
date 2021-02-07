7ba4296603763f041599d4cf1e00a907902e8c16
kkania44 <kaniakrzysztof99@gmail.com>
2021-02-07 22:36:53 +0100

    add logout feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from './auth/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Planner for your exam learing path';
+
+  constructor(
+    private tokenStorage: TokenStorageService,
+    private router: Router
+    ) {}
+
+  logout() {
+    this.tokenStorage.signOut();
+    this.router.navigateByUrl('/auth/login');
+  }
 }
